a6028d9fe1a0a825f5954c4bc0f3de5ad847640d
Shreyas Patel <shreyaspatel29@gmail.com>
2018-05-21 20:41:42 +0530

    Add HomeActivity

---
+++ app/src/main/java/com/letssolvetogether/omr/ui/activities/HomeActivity.java
@@ -0,0 +1,48 @@
+package com.letssolvetogether.omr.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.letssolvetogether.omr.main.R;
+import com.letssolvetogether.omr.ui.activities.camera.CameraActivity;
+
+public class HomeActivity extends AppCompatActivity{
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.scan_omr:
+                    Intent cameraIntent = new Intent(HomeActivity.this, CameraActivity.class);
+                    startActivity(cameraIntent);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
