bb26a2366024adcc62ba7ebac9b0689e205f9e61
Shreyas Patel <shreyaspatel29@gmail.com>
2018-08-13 23:40:54 +0530

    Add testcase

--- app/src/test/java/com/letssolvetogether/omr/main/OMRUnitTest.java
+++ app/src/test/java/com/letssolvetogether/omr/main/OMRUnitTest.java
@@ -1,9 +1,13 @@
 package com.letssolvetogether.omr.main;
 
+import com.letssolvetogether.omr.evaluation.EvaluationUtil;
+import com.letssolvetogether.omr.object.OMRSheet;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -18,16 +22,31 @@ import static org.junit.Assert.assertEquals;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class OMRUnitTest {
-    OMRTestActivity OMRTestActivity;
+    Mat matOMR;
+    OMRTestActivity omrTestActivity;
 
     @Before
     public void initOpenCV() {
         System.loadLibrary("opencv_java342");
-        OMRTestActivity = Robolectric.setupActivity(OMRTestActivity.class);
+        omrTestActivity = Robolectric.setupActivity(OMRTestActivity.class);
     }
 
     @Test
-    public void test(){
+    public void testOMRScore(){
         //OMRTestActivity is not added in manifest file
+        OMRSheet omrSheet = new OMRSheet();
+        omrSheet.setNumberOfQuestions(20);
+        omrSheet.setOmrSheetBlock();
+        omrSheet.setCorrectAnswers(new int[]{1,0,3,0,2,4,0,0,0,0,5,3,0,0,0,0,0,0,0,5});
+
+        for(int i=1;i<=3;i++){
+            matOMR = Imgcodecs.imread("testimages/omr_"+i+".jpg");
+            omrSheet.setMatOMRSheet(matOMR);
+            omrSheet.setWidth(matOMR.cols());
+            omrSheet.setHeight(matOMR.rows());
+            omrSheet.setOmrSheetBlock();
+            int score = new EvaluationUtil(omrSheet).getScore();
+            assertEquals(score,7);
+        }
     }
 }
