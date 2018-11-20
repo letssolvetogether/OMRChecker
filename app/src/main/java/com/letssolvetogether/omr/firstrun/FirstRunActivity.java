6217fdd44547f0735b1a88113240d1fa32c24727
Shreyas Patel <shreyaspatel29@gmail.com>
2018-11-20 21:01:07 +0530

    Add Tutorial Activity

---
+++ app/src/main/java/com/letssolvetogether/omr/firstrun/FirstRunActivity.java
@@ -0,0 +1,37 @@
+package com.letssolvetogether.omr.firstrun;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.letssolvetogether.omr.tutorial.TutorialActivity;
+import com.letssolvetogether.omr.home.ui.HomeActivity;
+
+public class FirstRunActivity extends AppCompatActivity {
+
+    private static final String FIRST_RUN = "first_run";
+
+    private SharedPreferences prefs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        if (prefs.getBoolean(FIRST_RUN, false)) {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            finish();
+        }else{
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(FIRST_RUN, true);
+            editor.apply();
+            Intent intent = new Intent(getApplicationContext(), TutorialActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
