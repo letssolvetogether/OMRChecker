7ee4a58de9f3438a2a1062a87069d274dd1e6392
Shreyas Patel <shreyaspatel29@gmail.com>
2018-12-08 11:25:57 +0530

    Change expiry date to March 30, 2019

--- app/src/main/java/com/letssolvetogether/omr/truetime/InitTrueTimeAsyncTask.java
+++ app/src/main/java/com/letssolvetogether/omr/truetime/InitTrueTimeAsyncTask.java
@@ -68,7 +68,7 @@ public class InitTrueTimeAsyncTask extends AsyncTask<Void, Void, Void> {
             Log.i(TAG,"TrueTime Intialized - " + date);
 
             //Caution: if lastFreeDate and trueTimeDate is same, it will exit.
-            Date lastFreeDate = new Date(118,11,31);  //Dec 30, 2018
+            Date lastFreeDate = new Date(119,2,31);  //March 30, 2019
             boolean validPeriodExpired = trueTimeDate.before(lastFreeDate);
 
             if(!validPeriodExpired){
